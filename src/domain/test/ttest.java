7b98ce2e617f10c72ac10320b1cc4f828ab243ff
liuting <liuting0109@126.com>
2020-08-14 13:56:31 +0800

    二叉搜索树排序数组

--- src/domain/test/ttest.java
+++ src/domain/test/ttest.java
@@ -1,5 +1,62 @@
 package domain.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/*class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        this.val = x;
+    }
+}*/
+class TreeNode {
+    //左子节点
+    private TreeNode leftNode;
+
+    //右子节点
+    private TreeNode rightNode;
+
+    //节点值
+    private Object value;
+
+    /**
+     * 添加节点(插入数据)
+     */
+    public void add(Object v){
+        //如果当前节点没有值，就把值放到此节点上
+        if (value == null)
+            value = v;
+        else {
+            //如果当前节点有值，根据大小关系决定放在左子节点还是右子节点
+            //新增的值比当前的值小或者相同
+            if ((Integer)v - (Integer)value < 0){
+                if (leftNode == null)
+                    leftNode = new TreeNode();
+                leftNode.add(v);
+            }else {
+                if (rightNode == null)
+                    rightNode = new TreeNode();
+                rightNode.add(v);
+            }
+        }
+    }
+
+    public List<Object> listValues(){
+        List<Object> res = new ArrayList<>();
+        //递归所有左节点
+        if (leftNode != null){
+            res.addAll(leftNode.listValues());
+        }
+        res.add(value);
+        if (rightNode != null){
+            res.addAll(rightNode.listValues());
+        }
+        return res;
+    }
+
+}
 public class ttest {
     public static void main(String[] args) {
         int[] arr = {3,2,4,6,8,7};
@@ -14,7 +71,35 @@ public class ttest {
         int[] arr1 = {1,2,3,4,6};
         int res = findMissNum(arr1);
         System.out.println(res);
+        System.out.println("++++++++++++++++++++++++");
+
+        int[] arr2 = {3,2,1,4,6,5};
+        TreeNode res2 = new TreeNode();
+        for (int n: arr2){
+            res2.add(n);
+        }
+        System.out.println(res2.listValues());
+
     }
+    //Java实现二叉树并利用遍历排序数组
+
+    /*public static TreeNode sortedArrayToBST(int[] nums){
+        if (nums == null ||nums.length ==0)
+            return null;
+        return solve(nums, 0 ,nums.length - 1);
+    }
+
+    private static TreeNode solve(int[] nums, int start, int end) {
+        if (start <= end){
+            int mid = start +(end - start)/2;
+            TreeNode root = new TreeNode(nums[mid]);
+            root.left = solve(nums,start, mid - 1);
+            root.right = solve(nums, mid +1 ,end);
+            return root;
+        }
+        return null;
+    }*/
+
 
     //1-n个数只有n-1个数找出缺失的那个数
     public static int findMissNum(int[] arr){
